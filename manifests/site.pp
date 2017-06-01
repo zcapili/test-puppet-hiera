c96dd46e2f8a25929ef7ebbde95b217a4bc7f1ea
zyra capili <zcapili@cambridge.org>
2017-06-01 11:28:32 +0800

    committed manifest

---
+++ manifests/site.pp
@@ -0,0 +1,35 @@
+node 'zy-puppetmaster.buildandrelease.org' {
+	class { 'ntp':
+		servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
+	}
+}
+
+node 'zy-puppetslave.buildandrelease.org' {
+	include '::ntp'
+	user { 'zcapili' :
+		ensure => 'present',
+	}
+	package { 'nano':
+		ensure => '2.3.1-10.el7',
+	}
+	user { 'read only user' :
+		name => 'voyuer',
+                ensure => 'present',
+        }
+	class { 'profile::release_accounts' :
+		ensure => 'present',
+
+	 }
+	include ::role::webserver
+}
+
+node 'zy-wordpress1.buildandrelease.org' {
+	include '::ntp'
+	include ::role::amp
+	include ::profile::wordpress
+}
+
+node 'zy-wordpress2.buildandrelease.org' {
+	include ::role::amp
+	include ::profile::wordpress
+}
